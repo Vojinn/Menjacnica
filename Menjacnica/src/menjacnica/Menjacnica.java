17ac3ba33cb3dcf6a8d54b4b70665087f7fc115c
Vojinn <dvojinovic38@gmail.com>
2018-03-19 20:12:01 +0100

    Popunjena tela metoda klase Menjacnica- do kraja implementiran interfejs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,27 +1,48 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
-
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
+	
 
 	@Override
 	public void dodajKursValuteZaDan(Kurs kurs, String nazivValute) {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < valute.size(); i++) {
+			if(nazivValute.equals(valute.get(i).getNaziv()))
+				kursevi.add(kurs);
+		}
 
 	}
 
 	@Override
 	public void brisanjeKursaValuteZaDan(GregorianCalendar datum, String nazivValute) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getNaziv().equals(nazivValute)) {
+				for (int j = 0; j < kursevi.size(); j++) {
+					if(kursevi.get(j).getDatum().equals(datum))
+						kursevi.remove(j);
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public Kurs vracanjeKursaValuteZaDan(GregorianCalendar datum, String nazivValute) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getNaziv().equals(nazivValute))
+				for (int j = 0; j < kursevi.size(); j++) {
+					if(kursevi.get(j).getDatum().equals(datum))
+						return kursevi.get(j);
+				}
+		}
 		return null;
 	}
 
